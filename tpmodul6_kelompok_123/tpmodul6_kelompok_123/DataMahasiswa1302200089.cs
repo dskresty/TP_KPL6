8f145d44c863c87f9e8e86c41da3ed91b946669a
FIRMAN PANJI <fpu2468@gmail.com>
2022-04-01 22:47:15 +0700

    menambahkan class <Panji>

---
+++ tpmodul6_kelompok_123/tpmodul6_kelompok_123/DataMahasiswa1302200089.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul6_kelompok_123
+{
+    internal class DataMahasiswa1302200089
+    {
+        public static void ReadJson()
+        {
+            //baca file json
+            String jsonString = File.ReadAllText("C:/Users/Panji/Documents/VS Community things/timResty/TP_KPL6/tpmodul6_kelompok_123/tpmodul6_kelompok_123/tp6_1_1302200089.json");
+
+            //Deserealize file json jadi object
+            dynamic mahasiswa = JsonConvert.DeserializeObject(jsonString);
+            //ntahlah
+            Console.WriteLine("Nama " + mahasiswa.nama.depan + " " + mahasiswa.nama.belakang + " dengan NIM " + mahasiswa.nim + " dari fakultas " + mahasiswa.fakultas);
+        }
+    }
+}
